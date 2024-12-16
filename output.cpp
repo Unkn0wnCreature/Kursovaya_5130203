4db0f1b7fdeb4bae78c9c2cad023309bfd42a6a1
soso4ka2012 <gawriil.vasin@gmail.com>
2024-12-16 20:05:40 +0300

    output.cpp

--- output.cpp
+++ output.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 #include "client.h"
 #include "book.h"
+#include "order.h"
 
 void print_file_client(const char file_name[30], struct Client c)
 {
@@ -112,4 +113,68 @@ void print_books_file(struct Book *list_books)
     {
         print_file_book("book.txt", list_books[i]);
     };
+}
+
+// Функция для записи отдельного заказа в файл
+void print_file_order(const char file_name[30], struct Order o)
+{
+    FILE *file;
+    char file_path[100];
+
+    strcpy(file_path, "data/");
+    strcat(file_path, file_name);
+
+    file = fopen(file_path, "a");
+
+    if (file)
+    {
+        fprintf(file, "%d %d %d %.2f\n", o.code, o.client.code, o.num_details, o.total);
+
+        // Сохранение всех деталей заказа
+        for (int i = 0; i < o.num_details; i++) {
+            fprintf(file, "  %s %d %.2f\n",
+                    o.details[i].book.title,
+                    o.details[i].quantity,
+                    o.details[i].subtotal);
+        }
+    }
+    else {
+        cout << "Error: Could not open file " << file_name << "\n";
+    }
+
+    fclose(file);
+}
+
+// Функция для записи общего количества заказов
+void print_num_orders(const char file_name[30], int num_orders)
+{
+    FILE *file;
+    char file_path[100];
+
+    strcpy(file_path, "data/");
+    strcat(file_path, file_name);
+
+    file = fopen(file_path, "w");
+
+    if (file) {
+        fprintf(file, "%d\n", num_orders);
+    }
+    else {
+        cout << "Error: Could not open file " << file_name << "\n";
+    }
+
+    fclose(file);
+}
+
+// Функция для записи всех заказов в файл
+void print_orders_file(struct Order *list_orders)
+{
+    clean_file("order.txt");
+
+    print_num_orders("order.txt", get_num_orders());
+
+    for (int i = 0; i < get_num_orders(); i++)
+    {
+        print_file_order("order.txt", list_orders[i]);
+    }
 }
