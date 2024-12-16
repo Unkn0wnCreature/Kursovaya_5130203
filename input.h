0d7a78dfc07338bc852a3229a403c688aafaddb4
soso4ka2012 <gawriil.vasin@gmail.com>
2024-12-16 21:37:25 +0300

    input.h

--- input.h
+++ input.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "client.h"
 #include "book.h"
+#include "order.h"
+
 
 void read_file_client(const char file_name[30], struct Client *list_clients);
 void print_client_console();
 
 void read_file_book(const char file_name[30], struct Book *list_books);
+
+void read_file_order(const char file_name[30], struct Order *list_orders, struct Client *list_clients, struct Book *list_books);
