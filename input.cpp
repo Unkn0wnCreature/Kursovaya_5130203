6399fdb0902189a33ac2df4c56baf96811aca361
soso4ka2012 <gawriil.vasin@gmail.com>
2024-12-16 22:36:17 +0300

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -69,40 +69,58 @@ void read_file_order(const char file_name[30], struct Order *list_orders, struct
     strcat(file_path, file_name);
 
     file = fopen(file_path, "r");
+    if (!file) {
+        cout << "Error: Could not open file " << file_name << "\n";
+        return;
+    }
 
-    if (file) {
-        fscanf(file, "%d\n", &num_orders);
+    // Считываем общее количество заказов
+    if (fscanf(file, "%d\n", &num_orders) != 1) {
+        cout << "Error: Could not read number of orders from file.\n";
+        fclose(file);
+        return;
+    }
 
-        for (int i = 0; i < num_orders; i++) {
-            int client_code, num_details;
-            fscanf(file, "%d %d %d %lf\n", &(list_orders[i].code), &client_code, &num_details, &(list_orders[i].total));
+    // Считываем каждый заказ
+    for (int i = 0; i < num_orders; i++) {
+        int client_code, num_details;
+        if (fscanf(file, "%d %d %d %lf\n", &(list_orders[i].code), &client_code, &num_details, &(list_orders[i].total)) != 4) {
+            cout << "Error: Could not read order details for order " << (i + 1) << ".\n";
+            fclose(file);
+            return;
+        }
 
-            // Сопоставление клиента по коду
-            int client_index = search_client_by_code(client_code, list_clients);
-            if (client_index != -1) {
-                list_orders[i].client = list_clients[client_index];
+        // Ищем клиента по коду
+        int client_index = search_client_by_code(client_code, list_clients);
+        if (client_index != -1) {
+            list_orders[i].client = list_clients[client_index];
+        } else {
+            cout << "Warning: Client with code " << client_code << " not found.\n";
+        }
+
+        // Считываем детали заказа
+        list_orders[i].num_details = num_details;
+        for (int j = 0; j < num_details; j++) {
+            char book_title[40];
+            int quantity;
+            double subtotal;
+
+            if (fscanf(file, " %39s %d %lf\n", book_title, &quantity, &subtotal) != 3) {
+                cout << "Error: Could not read order detail " << (j + 1) << " for order " << (i + 1) << ".\n";
+                fclose(file);
+                return;
             }
 
-            // Чтение деталей заказа
-            list_orders[i].num_details = num_details;
-            for (int j = 0; j < num_details; j++) {
-                char book_title[40];
-                int quantity;
-                double subtotal;
-
-                fscanf(file, "  %s %d %lf\n", book_title, &quantity, &subtotal);
-
-                // Сопоставление книги по названию
-                int book_index = search_book(book_title, list_books);
-                if (book_index != -1) {
-                    list_orders[i].details[j].book = list_books[book_index];
-                    list_orders[i].details[j].quantity = quantity;
-                    list_orders[i].details[j].subtotal = subtotal;
-                }
+            // Ищем книгу по названию
+            int book_index = search_book(book_title, list_books);
+            if (book_index != -1) {
+                list_orders[i].details[j].book = list_books[book_index];
+                list_orders[i].details[j].quantity = quantity;
+                list_orders[i].details[j].subtotal = subtotal;
+            } else {
+                cout << "Warning: Book with title \"" << book_title << "\" not found.\n";
             }
         }
-    } else {
-        cout << "Error: Could not open file " << file_name << "\n";
     }
 
     fclose(file);
