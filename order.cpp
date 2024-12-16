be5ae1957127d07446da9d80c60eb38ebbe829e4
soso4ka2012 <gawriil.vasin@gmail.com>
2024-12-16 17:26:29 +0300

    order.cpp

--- order.cpp
+++ order.cpp
@@ -3,55 +3,70 @@
 #include <cstring>
 #include <cstdio>
 #include "order.h"
-#include "book.h"
-#include "client.h"
+
 using namespace std;
 
-int num_orders = 0;
-static int index = 0;
+static int num_orders = 0; // Количество заказов
+static int order_index = 0; // Уникальный идентификатор заказа
 
+// Меню для управления заказами
 void menu_orders(int *opt, Client *list_clients, Book *list_books, Order *list_orders) {
     do {
         cout << "\n--- ORDER MANAGEMENT ---\n";
         cout << "[1] Insert Order\n";
         cout << "[2] Print Orders\n";
-        cout << "[3] Exit to Main Menu\n";
+        cout << "[3] Delete Order\n";
+        cout << "[4] Exit to Main Menu\n";
         cout << "Choose an option: ";
         cin >> *opt;
 
         switch (*opt) {
             case 1:
-                insert_order(&list_orders[get_num_orders()], list_clients, list_books);
+                insert_order(&list_orders[num_orders], list_clients, list_books);
+                num_orders++;
                 break;
             case 2:
-                print_list_orders(list_orders);
+                print_list_orders(list_orders, num_orders);
                 break;
-            case 3:
+            case 3: {
+                int code;
+                cout << "Enter Order Code to delete: ";
+                cin >> code;
+                int index = search_order(list_orders, num_orders, code);
+                if (index != -1) {
+                    delete_order(list_orders, &num_orders, index);
+                } else {
+                    cout << "Order not found.\n";
+                }
+                break;
+            }
+            case 4:
                 cout << "Exiting to Main Menu...\n";
                 break;
             default:
                 cout << "Invalid option. Try again.\n";
         }
-    } while (*opt != 3);
+    } while (*opt != 4);
 }
 
+// Вставка нового заказа
 void insert_order(Order *ord, Client *list_clients, Book *list_books) {
     char client_name[50];
-    int client_pos;
+    int client_index;
 
     cout << "\n--- INSERT NEW ORDER ---\n";
-    index++;
-    ord->code = index;
+    order_index++;
+    ord->code = order_index;
 
     cout << "Enter Client Name: ";
     cin >> client_name;
-    client_pos = search_client(client_name, list_clients);
+    client_index = search_client(client_name, list_clients);
 
-    if (client_pos == -1) {
+    if (client_index == -1) {
         cout << "Client not found.\n";
         return;
     }
-    ord->client = get_client(client_pos, list_clients);
+    ord->client = get_client(client_index, list_clients);
 
     char cont;
     do {
@@ -64,48 +79,52 @@ void insert_order(Order *ord, Client *list_clients, Book *list_books) {
             break;
         }
 
-        cout << "Add another order detail? (y/n): ";
+        cout << "Add another detail? (y/n): ";
         cin >> cont;
     } while (cont == 'y' || cont == 'Y');
 
-    ord->client.salary -= ord->total;
-    num_orders++;
+    cout << "Order inserted successfully.\n";
 }
 
-void insert_order_detail(OrderDetail *od, Book *list_books) {
+// Вставка новой детали заказа
+void insert_order_detail(OrderDetail *detail, Book *list_books) {
     char book_title[40];
-    int book_pos;
+    int book_index;
 
     cout << "\n--- INSERT ORDER DETAIL ---\n";
     cout << "Enter Book Title: ";
     cin >> book_title;
-    book_pos = search_book(book_title, list_books);
 
-    if (book_pos == -1) {
+    book_index = search_book(book_title, list_books);
+    if (book_index == -1) {
         cout << "Book not found.\n";
         return;
     }
-    od->book = get_book(book_pos, list_books);
+    detail->book = get_book(book_index, list_books);
 
     do {
         cout << "Enter Quantity: ";
-        cin >> od->quantity;
-        if (od->quantity > od->book.stock) {
+        cin >> detail->quantity;
+        if (detail->quantity > detail->book.stock) {
             cout << "Not enough stock available. Try again.\n";
         }
-    } while (od->quantity > od->book.stock);
+    } while (detail->quantity > detail->book.stock);
 
-    od->subtotal = od->quantity * od->book.price;
-    od->book.stock -= od->quantity;
+    detail->subtotal = detail->quantity * detail->book.price;
+    detail->book.stock -= detail->quantity;
+
+    cout << "Order detail added successfully.\n";
 }
 
-void print_list_orders(Order *list_orders) {
+// Печать списка всех заказов
+void print_list_orders(const Order *list_orders, int num_orders) {
     cout << "\n--- LIST OF ORDERS ---\n";
-    for (int i = 0; i < get_num_orders(); i++) {
+    for (int i = 0; i < num_orders; i++) {
         print_order(list_orders[i]);
     }
 }
 
+// Печать одного заказа
 void print_order(const Order &ord) {
     cout << "Order ID: " << ord.code << endl;
     cout << "Client: " << ord.client.name << endl;
@@ -123,6 +142,26 @@ void print_order(const Order &ord) {
     cout << "Total: " << ord.total << "\n\n";
 }
 
+// Удаление заказа
+void delete_order(Order *list_orders, int *num_orders, int order_index) {
+    for (int i = order_index; i < (*num_orders) - 1; i++) {
+        list_orders[i] = list_orders[i + 1];
+    }
+    (*num_orders)--;
+    cout << "Order deleted successfully.\n";
+}
+
+// Поиск заказа по коду
+int search_order(const Order *list_orders, int num_orders, int order_code) {
+    for (int i = 0; i < num_orders; i++) {
+        if (list_orders[i].code == order_code) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Получение количества заказов
 int get_num_orders() {
     return num_orders;
 }
